e89ad2c897a2f61e6df715547d2c8f229838c68f
pavanpandya <pavanpandya1311@gmail.com>
2024-04-19 10:28:46 -0400

    Pushing Common Utils Functionality

--- common/models.py
+++ common/models.py
@@ -1,22 +1,3 @@
-import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
 
-# Create your models here
-class Gender(models.TextChoices):
-    FEMALE = "female",
-    MALE = "male",
-    OTHER = "other"
-
-
-class BloodType(models.TextChoices):
-    A_POSITIVE = "A+",
-    A_NEGATIVE = "A-",
-    B_POSITIVE = "B+",
-    B_NEGATIVE = "B-",
-    O_POSITIVE = "O+",
-    O_NEGATIVE = "O-",
-    AB_POSITIVE = "AB+",
-    AB_NEGATIVE = "AB-",
-    UNKNOWN = "UNK"
+# Create your models here.
